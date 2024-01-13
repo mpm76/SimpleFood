7104f94a40ee1934097efe3a84d3126e2e128da0
mpm76 <mpmviolin@gmail.com>
2024-01-13 15:52:24 +0200

    added page index

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function (params) {
-  
+  $('.slider__inner').slick({
+    dots: true,
+    prevArrow: '<button type = "button"class="slick-prev"><img src="images/icons/prev-arrow.svg" alt="arrow-left">Previous</button>',
+    nextArrow: '<button type = "button"class="slick-next"><img src="images/icons/next-arrow.svg" alt="arrow-right"></button>',
+
+    // fade: true
+    // autoplay: true,
+    // autoplaySpeed: 2000
+  });
 });
