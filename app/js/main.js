e8bceb88f9627c97f984c830a4d72b1360c76ff0
mpm76 <mpmviolin@gmail.com>
2024-01-27 15:45:07 +0200

    added works interactiv

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,71 @@
 $(function (params) {
 
+  $('.product-tabs__top-item').on('click', function(e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active')
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+
+  $('.product-page__slider').slick({
+    dots: false,
+    prevArrow: '<button type = "button"class="slick-prev"><img src="images/icons/icon-arrow-right.svg" alt="arrow-left"></button>',
+    nextArrow: '<button type = "button"class="slick-next"><img src="images/icons/icon-arrow-left.svg" alt="arrow-right"></button>',
+  });
+
   $('.filter-price__input').ionRangeSlider({
     type: "double",
     prefix: "₴"
   });
 
-  $('.select-style').styler();
+  $('.select-style, .product-page__item-num').styler();
   
 
   $('.slider__inner').slick({
     dots: true,
     prevArrow: '<button type = "button"class="slick-prev"><img src="images/icons/prev-arrow.svg" alt="arrow-left">Previous</button>',
     nextArrow: '<button type = "button"class="slick-next"><img src="images/icons/next-arrow.svg" alt="arrow-right"></button>',
-
-    // fade: true
-    // autoplay: true,
-    // autoplaySpeed: 2000
   });
+
+    $(".product-page__item-star").rateYo({
+      starWidth: "16px",
+      normalFill: "#C1C1C1",
+      ratedFill: "#FFB800",
+      readOnly: true,
+      spacing: "6px"
+    });
+
+    $(".product-tabs__reviews-1--star").rateYo({
+      starWidth: "16px",
+      normalFill: "#C1C1C1",
+      ratedFill: "#FFB800",
+      readOnly: true,
+      spacing: "6px"
+    });
+
+    $(".product-tabs__reviews-2--star").rateYo({
+      starWidth: "16px",
+      normalFill: "#C1C1C1",
+      ratedFill: "#FFB800",
+      readOnly: true,
+      spacing: "6px"  
+    });
+
+    $(".product-tabs__review-star--active").rateYo({
+      starWidth: "16px",
+      normalFill: "#C1C1C1",
+      ratedFill: "#FFB800",
+      readOnly: true,
+      spacing: "6px"
+    });
+
+  $('.product-interest__items-slider').slick({
+    dots: false,
+    prevArrow: '<button type = "button"class="slick-prev"><img src="images/icons/prev-arrow.svg" alt="arrow-left"></button>',
+    nextArrow: '<button type = "button"class="slick-next"><img src="images/icons/next-arrow.svg" alt="arrow-right"></button>',
+  });
+
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ function scripts() {
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
