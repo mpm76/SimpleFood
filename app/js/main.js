75f756fedfc86be7e9769b25b32b53d9d53cd06e
mpm76 <mpmviolin@gmail.com>
2024-02-09 11:16:38 +0200

    added adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -34,8 +34,8 @@ $(function (params) {
 
   $('.product-page__slider').slick({
     dots: false,
-    prevArrow: '<button type = "button"class="slick-prev"><img src="images/icons/icon-arrow-right.svg" alt="arrow-left"></button>',
-    nextArrow: '<button type = "button"class="slick-next"><img src="images/icons/icon-arrow-left.svg" alt="arrow-right"></button>',
+    prevArrow: '<button type = "button"class="slick-prev"><img src="images/icons/icon-arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow: '<button type = "button"class="slick-next"><img src="images/icons/icon-arrow-right.svg" alt="arrow-right"></button>',
   });
 
   $('.filter-price__input').ionRangeSlider({
